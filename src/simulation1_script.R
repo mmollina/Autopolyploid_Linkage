fd91d0f7163e8503ee12ffa423e0fe7eba199656
Marcelo Mollinari <mmollina@GMAIL.COM>
2018-12-01 23:28:35 -0500

    update: simulation 1

--- src/simulation1_script.R
+++ src/simulation1_script.R
@@ -11,20 +11,20 @@ for(i in 1:length(arg)){
 rm(arg)
 
 ##Simulation Arguments
- m<-4                    # ploidy level
- rec.frac<-0.01          # recombination fraction
- n.ind<-200              # number of individuals
- n.mrk<-10               # number of markers
- n.sim.start<-1          # simulation starting point
- n.sim.stop<-3           # simulation stop point
- seed.for.config<-140    # seed for simulating the linkage phase configuration
- thres.twopt<-3          # threshold for two-point analysis
- extend.tail<-10         # number of markers to be considered when using HMM
-                         # at the end of a linkage group
- thres.hmm<-1            # threshold for the HMM analysis
- tol<-0.1                # tolerance for the HMM
- tol.final<-0.001        # tolerance for the final map
- case<-3                 # Case simulated
+ # m<-4                    # ploidy level
+ # rec.frac<-0.01          # recombination fraction
+ # n.ind<-200              # number of individuals
+ # n.mrk<-10               # number of markers
+ # n.sim.start<-1          # simulation starting point
+ # n.sim.stop<-3           # simulation stop point
+ # seed.for.config<-140    # seed for simulating the linkage phase configuration
+ # thres.twopt<-3          # threshold for two-point analysis
+ # extend.tail<-10         # number of markers to be considered when using HMM
+ #                         # at the end of a linkage group
+ # thres.hmm<-1            # threshold for the HMM analysis
+ # tol<-0.1                # tolerance for the HMM
+ # tol.final<-0.001        # tolerance for the final map
+ # case<-3                 # Case simulated
 
 ##Which scenrario should be simulated
 if(case==1)
